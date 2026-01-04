433e143b3d5ac22d2a509f2a1c6313b59dbb7dbb
Obslink_19 <149531268+Obsi19@users.noreply.github.com>
2026-01-04 21:40:08 +0500

    Remove sensitive database credentials
    Removed database connection details for security reasons.

--- src/main/java/com/ObsList/AddDestinationServlet.java
+++ src/main/java/com/ObsList/AddDestinationServlet.java
@@ -12,9 +12,9 @@ public class AddDestinationServlet extends HttpServlet {
 
     
 	private static final long serialVersionUID = 1L;
-	private static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/travel_guide";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Obslink_19";
+	private static final String DB_URL = "";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
